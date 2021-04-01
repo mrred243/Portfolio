a45b0ed93b9288f98dad4bbaa73cd3e02295efe8
mrred243 <tranthie04@gmail.com>
2021-04-01 18:32:50 +0300

    edit cover page element postion and text color

--- src/views/CoverPage.js
+++ src/views/CoverPage.js
@@ -9,12 +9,12 @@ const CoverPage = () => {
     return (
         <div className="cover">
             <h1>Welcome to Thien-An's portfolio</h1>
-            <h2 style={{marginBottom: 20}}>But first, have a nice day.</h2>
+            <h3 style={{marginBottom: 20}}>But first, have a nice day.</h3>
             <div>
               <button className="intro__btn" onClick={() => history.push(`/home`)}>
               <div className="label" >
                 <span className="hover-effect"></span>
-                <span className="label-text">Continue &nbsp;&nbsp;<span style={{fontSize: '1.1em'}}>❯❯❯</span></span>
+                <span className="label-text">Continue <span>❯❯❯</span></span>
                 </div>                  
               </button>
             </div>
